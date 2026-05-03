0475d0baaa0cb5b7e96e35b957a0d84c134824c7
nicanordlc <nicanordlc.dev@gmail.com>
2026-05-03 14:52:48 -0400

    format sourceIfExists (by nvim)

--- src/utils/source-if-exists.sh
+++ src/utils/source-if-exists.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # shellcheck disable=1090,1117,2155
 
-__source_glob(){
-    for TARGET in "${@:?No files to be sourced...}"
-    do
+__source_glob() {
+    for TARGET in "${@:?No files to be sourced...}"; do
         local TARGET_BASENAME=$(basename "$TARGET")
-        local IS_IGNORED=$(grep -E "\b$TARGET_BASENAME\b" <<< "${IGNORED_FILES[*]}" 2> /dev/null)
+        local IS_IGNORED=$(grep -E "\b$TARGET_BASENAME\b" <<<"${IGNORED_FILES[*]}" 2>/dev/null)
 
         [[ "$IS_IGNORED" ]] && continue
-
 
         [ -f "$TARGET" ] && source "$TARGET"
     done
 }
 
-sourceIfExists(){ # Source file if exists (glob pattern)
+sourceIfExists() { # Source file if exists (glob pattern)
     local IGNORED_FILES=(
         .bash_profile
         .bash_history
@@ -22,19 +20,17 @@ sourceIfExists(){ # Source file if exists (glob pattern)
         Session.vim
     )
 
-    while getopts ":i:" OPT
-    do
+    while getopts ":i:" OPT; do
         case $OPT in
-            i)
-                IGNORED_FILES+=("$OPTARG")
-                ;;
-            \?)
-                echo "Invalid option (-$OPTARG)"
-                ;;
+        i)
+            IGNORED_FILES+=("$OPTARG")
+            ;;
+        \?)
+            echo "Invalid option (-$OPTARG)"
+            ;;
         esac
     done
-    shift $((OPTIND-1))
+    shift $((OPTIND - 1))
 
     __source_glob "$@"
 }
-
